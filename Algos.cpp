0736720859408ebf5c00b2eef0684cc60e4b02fd
Yousfi Abdelkader <95932250+sher0u@users.noreply.github.com>
2025-06-06 23:02:31 +0300

    Update Algos.cpp

--- Algos.cpp
+++ Algos.cpp
@@ -170,3 +170,80 @@ int main()
 }
 
 -------------------------------------------------------------------------------------------------
+#include <iostream>
+#include <list>
+#include <stack>
+using namespace std;
+
+class Graph {
+    int V;
+    list<int> *adj;
+
+    void topologicalSortUtil(int v, bool visited[], stack<int> &Stack);
+public:
+    Graph(int V);
+    void addEdge(int v, int w);
+    void topologicalSort();
+};
+
+Graph::Graph(int V) {
+    this->V = V;
+    adj = new list<int>[V];
+}
+
+void Graph::addEdge(int v, int w) {
+    adj[v].push_back(w);
+}
+
+void Graph::topologicalSortUtil(int v, bool visited[], stack<int> &Stack) {
+    visited[v] = true;
+
+    for (int neighbor : adj[v]) {
+        if (!visited[neighbor])
+            topologicalSortUtil(neighbor, visited, Stack);
+    }
+
+    Stack.push(v);
+}
+
+void Graph::topologicalSort() {
+    stack<int> Stack;
+    bool *visited = new bool[V];
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
+
+    for (int i = 0; i < V; i++)
+        if (!visited[i])
+            topologicalSortUtil(i, visited, Stack);
+
+    while (!Stack.empty()) {
+        cout << Stack.top() << " ";
+        Stack.pop();
+    }
+}
+
+int main() {
+    int V, E;
+    cout << "Enter the number of vertices: ";
+    cin >> V;
+
+    Graph g(V);
+
+    cout << "Enter the number of edges: ";
+    cin >> E;
+
+    cout << "Enter edges in the format: src dest\n";
+    cout << "(Note: vertices should be from 0 to " << V-1 << ")\n";
+    for (int i = 0; i < E; i++) {
+        int src, dest;
+        cout << "Edge " << i + 1 << ": ";
+        cin >> src >> dest;
+        g.addEdge(src, dest);
+    }
+
+    cout << "Topological Sort of the given graph:\n";
+    g.topologicalSort();
+
+    return 0;
+}
+-------------------------------------------------------------------------------------------------
